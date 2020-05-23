1cecc461045be4bb98365442a8f35a4131873328
Luis Gustavo Santos <gustavoluisrh@gmail.com>
2020-05-23 18:23:31 -0300

    Calculate total

--- src/routes/transaction.routes.ts
+++ src/routes/transaction.routes.ts
@@ -10,8 +10,12 @@ const transactionsRepository = new TransactionsRepository();
 transactionRouter.get('/', (request, response) => {
   try {
     const transaction = transactionsRepository.all();
+    const balance = transactionsRepository.getBalance();
 
-    return response.json(transaction);
+    return response.json({
+      transaction,
+      balance,
+    });
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
